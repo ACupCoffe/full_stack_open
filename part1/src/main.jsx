dab71d3773bdd7fc28bbfb301fe993aa0c4a8078
Ancieta Leandro <ancietalea.imp@gmail.com>
2025-05-19 10:32:30 -0300

    restauraion de previo archivos debido a error

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,18 +1,3 @@
-// // import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-
-// createRoot(document.getElementById("root")).render(
-//   // <StrictMode>
-//   //   <App />
-//   // </StrictMode>
-
-//   <App />
-// );
-
-// // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
